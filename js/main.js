91d602ea61518b96c3365b7297d4b67bfc53e173
Hidoi-Chan <HidoiChan@mail.ru>
2021-03-08 10:40:19 +0300

    jQuery UI touch-punch

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 $(document).ready(() => {
     const $fieldset = $('fieldset[name="target"]')
     const $radio = ($fieldset[0].elements)
-    let target = 'text'
+    let target = 'color'
 
     const $colorField = $('.field')
     const $thumbs = $('.slider__thumb')
     const $fields = $('.slider__field')
     const $fills = $('.slider__fill')
     const colors = {
-        text: [0, 0, 0],
-        background: [255, 255, 255]
+        color: [0, 0, 0],
+        'background-color': [255, 255, 255]
     }
     const $elemGetColor = $('#get-color')[0]
     const point = 255 / ($fields.outerWidth() - $thumbs.outerWidth())
@@ -45,12 +45,7 @@ $(document).ready(() => {
     })
 
     function setColor() {
-        if (target === 'text') {
-            $colorField.css('color', `rgb(${colors[target].join(', ')})`)
-        }
-        if (target === 'background') {
-            $colorField.css('background-color', `rgb(${colors[target].join(', ')})`)
-        }
+        $colorField.css(target, `rgb(${colors[target].join(', ')})`)
 
         getColorToText()
     }
@@ -58,7 +53,7 @@ $(document).ready(() => {
     function getColorToText() {
 
         $($elemGetColor).text(`rgb(${colors[target]
-            .map((item, index) => Math.round(item))
+            .map((item) => Math.round(item))
             .join(', ')})`
         )
     }
